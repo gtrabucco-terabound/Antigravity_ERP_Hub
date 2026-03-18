f5edb8c1d243367b0dfc8741bb3ca177955673e3
German Trabucco <gtrabucco@terabound.com>
2026-03-18 19:37:36 +0000

    estas configurando

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDqf2K1KLrri0o79irHQ6pE07VUsA0rrPk",
+  authDomain: "studio-1405627774-cebad.firebaseapp.com",
+  projectId: "studio-1405627774-cebad",
+  storageBucket: "studio-1405627774-cebad.firebasestorage.app",
+  messagingSenderId: "70936170464",
+  appId: "1:70936170464:web:f2ba4a94cbc809a4e02585"
+};
